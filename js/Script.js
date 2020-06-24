feaca13861b6b70154f3beea81b37afaae8fc60f
Sergey <lifeglobephoenixjw@gmail.com>
2020-06-24 15:09:13 +0300

    Практическая работа ч-1

--- js/Script.js
+++ js/Script.js
@@ -20,4 +20,22 @@
 
 // const user = "Ivan";
 // alert(`privet, ${user}`);
+const numberOfFilms = +prompt('Сколько фильмов вы уже просмотрели', '');
 
+const personalMovieDb = {
+	count: numberOfFilms,
+	movies: {},
+	actors: {},
+	genres: [],
+	privat: false
+};
+
+
+const a = prompt('Один из просмотренных фильмов', ''),
+	b = prompt('На сколько вы его оцените', ''),
+	c = prompt('Один из просмотренных фильмов', ''),
+	d = prompt('На сколько вы его оцените', '');
+
+personalMovieDb.movies[a] = b;
+personalMovieDb.movies[c] = d;
+console.log(personalMovieDb);
